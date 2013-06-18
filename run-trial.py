2d8c4d8a47a39dfddd672f7ef2e23e9f319ed53d
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-06-18 01:04:23 +0000

    Rename sender->server and receiver->client for web benchmark

--- run-trial.py
+++ run-trial.py
@@ -25,46 +25,46 @@ class ProtoTester(Topo):
         Topo.__init__(self)
 
         # Add hosts and switches
-        sender = self.addHost('sender', ip='10.0.1.1', mac='00:00:00:00:00:01')
+        server = self.addHost('server', ip='10.0.1.1', mac='00:00:00:00:00:01')
         LTE = self.addHost('LTE', ip='0.0.0.0')
-        receiver = self.addHost('receiver', ip='10.0.1.2', mac='00:00:00:00:00:02')
+        client = self.addHost('client', ip='10.0.1.2', mac='00:00:00:00:00:02')
 
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
 
         # Add links
-        self.addLink(sender, s1)
+        self.addLink(server, s1)
         self.addLink(s1, LTE)
         self.addLink(LTE, s2)
-        self.addLink(s2, receiver)
+        self.addLink(s2, client)
 
-def set_all_IP(net, sender, LTE, receiver):
-    sender.sendCmd('ifconfig sender-eth0 10.0.1.1 netmask 255.255.255.0')
-    sender.waitOutput()
+def set_all_IP(net, server, LTE, client):
+    server.sendCmd('ifconfig server-eth0 10.0.1.1 netmask 255.255.255.0')
+    server.waitOutput()
     LTE.sendCmd('ifconfig LTE-eth0 up')
     LTE.waitOutput()
     LTE.sendCmd('ifconfig LTE-eth1 up')
     LTE.waitOutput()
-    receiver.sendCmd('ifconfig receiver-eth0 10.0.1.2 netmask 255.255.255.0')
-    receiver.waitOutput()
+    client.sendCmd('ifconfig client-eth0 10.0.1.2 netmask 255.255.255.0')
+    client.waitOutput()
 
-    sender.sendCmd('echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6')
-    sender.waitOutput()
+    server.sendCmd('echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6')
+    server.waitOutput()
     LTE.sendCmd('echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6')
     LTE.waitOutput()
-    receiver.sendCmd('echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6')
-    receiver.waitOutput()
+    client.sendCmd('echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6')
+    client.waitOutput()
 
-def display_routes(net, sender, LTE, receiver):
-    print 'sender route...'
-    sender.sendCmd('route -n')
-    print sender.waitOutput()
+def display_routes(net, server, LTE, client):
+    print 'server route...'
+    server.sendCmd('route -n')
+    print server.waitOutput()
     print 'LTE route...'
     LTE.sendCmd('route -n')
     print LTE.waitOutput()
-    print 'receiver route...'
-    receiver.sendCmd('route -n')
-    print receiver.waitOutput()
+    print 'client route...'
+    client.sendCmd('route -n')
+    print client.waitOutput()
 
 def run_cellsim(LTE):
     LTE.sendCmd('/home/ubuntu/multisend/sender/cellsim-setup.sh LTE-eth0 LTE-eth1')
@@ -74,14 +74,14 @@ def run_cellsim(LTE):
     LTE.waitOutput()
     print "done."
 
-def run_datagrump(sender, receiver):
-    print "Running datagrump-receiver...",
-    receiver.sendCmd('/home/ubuntu/datagrump/datagrump-receiver 9000 >/tmp/receiver-stdout 2>/tmp/receiver-stderr &')
-    receiver.waitOutput()
+def run_datagrump(server, client):
+    print "Running datagrump-client...",
+    client.sendCmd('/home/ubuntu/datagrump/datagrump-receiver 9000 >/tmp/client-stdout 2>/tmp/client-stderr &')
+    client.waitOutput()
     print "done."
-    print "Running datagrump-sender...",
-    sender.sendCmd('/home/ubuntu/datagrump/datagrump-sender 10.0.1.2 9000 debug >/tmp/sender-stdout 2>/tmp/sender-stderr &')
-    sender.waitOutput()
+    print "Running datagrump-server...",
+    server.sendCmd('/home/ubuntu/datagrump/datagrump-sender 10.0.1.2 9000 debug >/tmp/server-stdout 2>/tmp/server-stderr &')
+    server.waitOutput()
     print "done."
 
 def print_welcome_message():
@@ -89,9 +89,9 @@ def print_welcome_message():
     print "#                                                                  #"
     print "#               6.829 PS 2 Emulated Network Test                   #"
     print "#                                                                  #"
-    print "#          running sender <=> cellsim <=> receiver                 #"
+    print "#          running server <=> cellsim <=> client                 #"
     print "#                                                                  #"
-    print "#  Debug output in /tmp/{sender,receiver,cellsim}-{stdout,stderr}  #"
+    print "#  Debug output in /tmp/{server,client,cellsim}-{stdout,stderr}  #"
     print "#                                                                  #"
     print "####################################################################"
     print
@@ -108,17 +108,17 @@ def run_cellsim_topology():
     net = Mininet(topo=topo, host=Host, link=Link)
     net.start()
 
-    sender = net.getNodeByName('sender')
+    server = net.getNodeByName('server')
     LTE = net.getNodeByName('LTE')
-    receiver = net.getNodeByName('receiver')
+    client = net.getNodeByName('client')
 
-    set_all_IP(net, sender, LTE, receiver)
+    set_all_IP(net, server, LTE, client)
     
     #Dump connections
     #dumpNodeConnections(net.hosts)
-    #display_routes(net, sender, LTE, receiver)
+    #display_routes(net, server, LTE, client)
 
-    run_datagrump(sender, receiver)
+    run_datagrump(server, client)
 
     run_cellsim(LTE)
 
